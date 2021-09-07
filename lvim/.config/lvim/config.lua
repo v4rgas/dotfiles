08c573c4af814bd4b40b00f3abb6ad03e7ab4bc2
v4rgas <juanvargas@uc.cl>
2021-09-07 08:50:25 -0300

    ahora veo dotfiles por defecto

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -45,6 +45,7 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.hide_dotfiles = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {}
