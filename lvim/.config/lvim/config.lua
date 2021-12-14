f1003f7be12597c645dacb39aefc100d304a618a
v4rgas <juanvargas@uc.cl>
2021-12-14 18:33:25 -0300

    idk

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -16,10 +16,17 @@ lvim.builtin.treesitter.ensure_installed = "maintained"
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
+lvim.lsp.automatic_servers_installation = true
+
+-- lvim.g['neotex_delay'] = 500
+-- lvim.builtin.neotex_enabled = 2
+
 -- additional Plugins
 lvim.plugins = {
   {'chriskempson/base16-vim'},
   {"lervag/vimtex"},
+  -- {'donRaphaco/neotex'},
+  -- {'xuhdev/vim-latex-live-preview'},
   {
     "iamcco/markdown-preview.nvim",
       ft = "markdown",
@@ -28,18 +35,20 @@ lvim.plugins = {
   },
 }
 
--- latex plugin config
-lvim.builtin.which_key.mappings["t"] = {
-    name = "+Latex",
-    c = { "<cmd>VimtexCompile<cr>", "Toggle Compilation Mode" },
-    f = { "<cmd>call vimtex#fzf#run()<cr>", "Fzf Find" },
-    i = { "<cmd>VimtexInfo<cr>", "Project Information" },
-    s = { "<cmd>VimtexStop<cr>", "Stop Project Compilation" },
-    t = { "<cmd>VimtexTocToggle<cr>", "Toggle Table Of Content" },
-    v = { "<cmd>VimtexView<cr>", "View PDF" },
-    b = { "<cmd>TexlabBuild<cr>", "Build with Texlab" },
-    p = { "<cmd>TexlabForward<cr>", "Preview with Texlab" },
-  }
+-- vim.g.livepreview_previewer = 'zathura'
+
+-- -- latex plugin config
+-- lvim.builtin.which_key.mappings["t"] = {
+--     name = "+Latex",
+--     c = { "<cmd>VimtexCompile<cr>", "Toggle Compilation Mode" },
+--     f = { "<cmd>call vimtex#fzf#run()<cr>", "Fzf Find" },
+--     i = { "<cmd>VimtexInfo<cr>", "Project Information" },
+--     s = { "<cmd>VimtexStop<cr>", "Stop Project Compilation" },
+--     t = { "<cmd>VimtexTocToggle<cr>", "Toggle Table Of Content" },
+--     v = { "<cmd>VimtexView<cr>", "View PDF" },
+--     b = { "<cmd>TexlabBuild<cr>", "Build with Texlab" },
+--     p = { "<cmd>TexlabForward<cr>", "Preview with Texlab" },
+--   }
 
 -- compile on initialization, cleanup on quit
   vim.api.nvim_exec(
